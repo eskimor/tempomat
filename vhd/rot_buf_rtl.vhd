a740a0e22228278cc98c319d95d81007701f31c2
Robert Klotzner <robert.klotzner@gmx.at>
2013-06-19 19:28:52 +0200

    Rot buf implemented.

--- vhd/rot_buf_rtl.vhd
+++ vhd/rot_buf_rtl.vhd
@@ -37,30 +37,35 @@ use ieee.numeric_std.all;
 library work;
 use work.comp_pack.all;
 
-architecture rot_knob_debounced of rot_knob_buffer is
+architecture rot_buf of rot_buf is
     
     signal rot_right : std_logic;
     signal rot_left : std_logic;
 begin
   read_value: process(clk_in, rst_in) is
     variable value : natural range 0 to 255;
-    variable doSleep : std_logic;
+    variable rot_a_old : std_logic;
+    variable rot_b_old : std_logic;
   begin
     if rst_in = '0'
     then
         value := 0;
         value_out <= (others <= '0');
-        doSleep := '0'
+        rot_a_old := rot_a_i;
+        rot_b_old := rot_b_i;
 
-    elsif clk_in'event and clk_in = '1' and doSleep /= '0' 
+    elsif clk_in'event and clk_in = '1' 
     then
-        if rot_a_i =     
-        addr := to_integer ( unsigned(addr_in) );
-
-    elsif doSleep = '1'
+        if rot_a_i = '1' and rot_a_old = '0' and rot_b_i = '0' and value /= 255
+        then
+            value := value + 1;
+        elsif rot_b_i = '1' and rot_b_old = '0' and rot_a_i = '0' and value /= 0
+        then
+            value := value - 1;
+        end if;
+        rot_a_old := rot_a_in;
+        rot_b_old := rot_b_in;
+        value_out <= data_t(to_unsigned(value, data_t'length));
     end if;
-  end process mem_cycle;
-  detect_value: process(rot_a_i, rot_b_i) is
-  begin
-  end process detect_value;
-end impl;
+  end process read_value;
+end rot_buf;
